9799934eba6aff73ca7ecffad01938fb96c9c9df
GIGROUP\tsobota <tomas.sobota@verisoft.cz>
2025-03-26 20:00:31 +0100

    add appsetting

--- PortfolioManager/BuilderExtensions/MinimalApiExtension.cs
+++ PortfolioManager/BuilderExtensions/MinimalApiExtension.cs
@@ -1,45 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using PortfolioManager.Extensions;
-using PortfolioManager.Managers.Services.Interfaces;
-using PortfolioManager.Managers.ToolServices.Interfaces;
-using PortfolioManager.Models.Models.User;
+﻿    using Microsoft.AspNetCore.Mvc;
+    using PortfolioManager.Extensions;
+    using PortfolioManager.Managers.Services.Interfaces;
+    using PortfolioManager.Managers.ToolServices.Interfaces;
+    using PortfolioManager.Models.Models.User;
 
-namespace PortfolioManager.BuilderExtensions;
+    namespace PortfolioManager.BuilderExtensions;
 
-public static class MinimalApiExtension
-{
-    public static void UseMinimalApi(this WebApplication app)
+    public static class MinimalApiExtension
     {
-        var authGroup = app.MapGroup("/auth").WithTags("Auth");
-        var toolsGroup = app.MapGroup("/tools").WithTags("Tools");
-
-        authGroup.MapPost("/register", async (RegisterUser registerUser, IAuthService authService) =>
+        public static void UseMinimalApi(this WebApplication app)
         {
-            var result = await authService.RegisterUserAsync(registerUser);
-            return result.ConvertToResult();
-        });
+            var authGroup = app.MapGroup("/auth").WithTags("Auth");
+            var toolsGroup = app.MapGroup("/tools").WithTags("Tools");
 
-        authGroup.MapPost("/login", async (LoginUser loginUser, IAuthService authService) =>
-        {
-            var result = await authService.LoginUserAsync(loginUser);
-            return result.ConvertToResult();
-        });
+            authGroup.MapPost("/register", async (RegisterUser registerUser, IAuthService authService) =>
+            {
+                var result = await authService.RegisterUserAsync(registerUser);
+                return result.ConvertToResult();
+            });
 
-        authGroup.MapPost("/logout", async (IAuthService authService) =>
-        {
-            //TODO: token blacklist v cache
-        });
+            authGroup.MapPost("/login", async (LoginUser loginUser, IAuthService authService) =>
+            {
+                var result = await authService.LoginUserAsync(loginUser);
+                return result.ConvertToResult();
+            });
 
-        toolsGroup.MapPost("/checkPersonalIdentificationNumber", (IPersonalIdentificationNumberValidationService personalIdentificationNumberValidationService, [FromBodyAttribute] string text) =>
-        {
-            var result = personalIdentificationNumberValidationService.Validate(text);
-            return result.ConvertToResult();
-        });
+            authGroup.MapPost("/logout", async (IAuthService authService) =>
+            {
+                //TODO: token blacklist v cache
+            });
 
-        toolsGroup.MapPost("/checkCompanyId", async (ICompanyIdValidationService companyIdValidationService, [FromBodyAttribute] string text) =>
-        {
-            var result = await companyIdValidationService.ValidateAsync(text);
-            return result.ConvertToResult();
-        });
+            toolsGroup.MapPost("/checkPersonalIdentificationNumber", (IPersonalIdentificationNumberValidationService personalIdentificationNumberValidationService, [FromBodyAttribute] string text) =>
+            {
+                var result = personalIdentificationNumberValidationService.Validate(text);
+                return result.ConvertToResult();
+            });
+
+            toolsGroup.MapPost("/checkCompanyId", async (ICompanyIdValidationService companyIdValidationService, [FromBodyAttribute] string text) =>
+            {
+                var result = await companyIdValidationService.ValidateAsync(text);
+                return result.ConvertToResult();
+            });
+        }
     }
-}
