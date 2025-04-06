ca0e13b4568dae6783a2e5362869c6132df2ec85
GIGROUP\tsobota <tomas.sobota@verisoft.cz>
2025-04-06 19:43:23 +0200

    test

--- PortfolioManager.Tests/PhoneNumberValidationServiceTests.cs
+++ PortfolioManager.Tests/PhoneNumberValidationServiceTests.cs
@@ -73,5 +73,5 @@ public class PhoneNumberValidationServiceTests
         Assert.IsNull(result.Data.E164Format);
         Assert.IsNull(result.Data.NationalFormat);
         Assert.IsNull(result.Data.InternationalFormat);
-    }
+    }//test
 }
