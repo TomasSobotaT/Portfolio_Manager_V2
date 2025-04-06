0d3e84607fc2c74ee40bb533a07af995bb3ed4d8
GIGROUP\tsobota <tomas.sobota@verisoft.cz>
2025-04-06 18:20:14 +0200

    add cz phone validation tests

--- PortfolioManager.Tests/PhoneNumberValidationServiceTests.cs
+++ PortfolioManager.Tests/PhoneNumberValidationServiceTests.cs
@@ -1,4 +1,5 @@
 ﻿using AutoFixture;
+using AutoFixture.Kernel;
 using PortfolioManager.Managers.Tools;
 using PortfolioManager.Managers.Tools.Interfaces;
 
@@ -15,14 +16,62 @@ public class PhoneNumberValidationServiceTests
     {
         phoneNumberValidationService = new PhoneNumberValidationService();
         fixture = new Fixture();
-        fixture.Customizations.Add(new RandomNumericSequenceGenerator(100_000_000, 999_999_999));
     }
 
     [Test]
-    public void Validate_ShouldWork()
+    public void Validate_CzTest()
     {
-        int fakePhoneNumber = fixture.Create<int>();
-        var result = phoneNumberValidationService.Validate(fakePhoneNumber.ToString(), Base.Enums.Countries.CZ);
-        Assert.That(result.Data.IsValid, Is.EqualTo(true));
+        var generator = new RandomNumericSequenceGenerator(100_000_000, 999_999_999);
+        var phoneNumber = (int)generator.Create(typeof(int), new SpecimenContext(fixture));
+        var result = phoneNumberValidationService.Validate(phoneNumber.ToString(), Base.Enums.Countries.CZ);
+
+        if (result.Data.IsValid)
+        {
+            Assert.IsTrue(result.Data.IsValid);
+            Assert.That(result.Data.E164Format[0..4], Is.EqualTo("+420"));
+            Assert.That(result.Data.E164Format.Length, Is.EqualTo(13));
+            Assert.That(result.Data.NationalFormat.Length, Is.EqualTo(11));
+            Assert.That(result.Data.Region, Is.EqualTo("Czechia"));
+            Assert.That(result.Data.Message, Is.EqualTo("The phone number is valid."));
+        }
+        else
+        {
+            Assert.IsFalse(result.Data.IsValid);
+            Assert.That(result.Data.InternationalFormat, Is.Null);
+            Assert.That(result.Data.E164Format, Is.Null);
+            Assert.That(result.Data.NationalFormat, Is.Null);
+        }
+    }
+
+    [Test]
+    public void Validate_CzShouldWork()
+    {
+        var phoneNumbers = new List<string>(["420123456789", "732874569", "+420987654321", "383985412", "+420383741741"]);
+
+        var phoneNumber = phoneNumbers[Random.Shared.Next(5)];
+
+        var result = phoneNumberValidationService.Validate(phoneNumber.ToString(), Base.Enums.Countries.CZ);
+
+        Assert.IsTrue(result.Data.IsValid);
+        Assert.That(result.Data.E164Format, Is.EqualTo($"+420{phoneNumber}"));
+        Assert.That(result.Data.E164Format.Length, Is.EqualTo(13));
+        Assert.That(result.Data.NationalFormat.Length, Is.EqualTo(11));
+        Assert.That(result.Data.Region, Is.EqualTo("Czechia"));
+        Assert.That(result.Data.Message, Is.EqualTo("The phone number is valid."));
+     }
+
+    [Test]
+    public void Validate_CzShoulNotWork()
+    {
+        var phoneNumbers = new List<string>(["420s23456789", "1732874569", "+4209876543210", "383985412FF", "+42038rR41741"]);
+
+        var phoneNumber = phoneNumbers[Random.Shared.Next(5)];
+
+        var result = phoneNumberValidationService.Validate(phoneNumber.ToString(), Base.Enums.Countries.CZ);
+
+        Assert.IsFalse(result.Data.IsValid);
+        Assert.IsNull(result.Data.E164Format);
+        Assert.IsNull(result.Data.NationalFormat);
+        Assert.IsNull(result.Data.InternationalFormat);
     }
 }
